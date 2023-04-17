5c5b89e96f630d8a82f91b72d5524334277ba0aa
nrtimms <nataliertimms18@gmail.com>
2023-04-16 20:56:34 -0700

    added subclass

--- mercurySedan.js
+++ mercurySedan.js
@@ -12,24 +12,62 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+            this.maximumPassengers = 5;
+            this.passengers = 0;
+            this.numberOfWheels = 4;
+            this.maximumSpeed = 160;
+            this.fuel = 10;
+            this.scheduleService = false;
+    }
 
+loadPassenger(num){
+    if(this.passengers < this.maximumPassengers){
+        if((this.passengers + num) <= this.maximumPassengers){
+            this.passengers = num;
+            return this.passengers;
+        }
+        else{
+            console.log("This " + this.make + " " + this.model + " can only fit " + this.maximumPassengers + " passengers.");
+        }
+    }   
+    else{
+        console.log("This " + this.make + " " + this.model + " is full.");
+    }
+}
 
+start(){
+    if(this.fuel > 0){
+        console.log("The car has started.");
+        this.started = true;
+        return this.started;
+    }
+    else{
+        console.log("The car has no fuel");
+        this.started = false;
+        return this.started;
+    }
+}
 
+checkMileage(mileage){
+    if(mileage > 30000){
+        this.scheduleService = true;
+        return this.scheduleService;
+    }
+}
 
-
-
-
-
-
-
-
+}
 
 //TO DO: Creating Instances and Testing Them
+let aCar =  new Car('Mercury', "Sedan", '2017', 'Blue', 40000)
+aCar.loadPassenger(4)
+aCar.start()
+aCar.checkMileage()
+console.log(aCar)
 
 //You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
 
 
 //Create at least two new instances of the Car class and test them here:
